f3f57b50b303b0187db6acf3415c85907cdd58f2
RoyalReject <cs0888866@otc.edu>
2022-04-14 23:24:01 -0500

    Init project

---
+++ src/main/java/com/rrstudio/mods/CrystalQuarry.java
@@ -0,0 +1,8 @@
+package com.rrstudio.mods;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CrystalQuarry.MODID)
+public class CrystalQuarry {
+    public static final String MODID = "crystalquarry";
+}
